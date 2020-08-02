235c5e723be116c90399e9e5804246e2be32c877
root <root@localhost.localdomain>
2020-08-02 08:15:15 -0400

    add common linkTable.py

--- module/messagequeue.py
+++ module/messagequeue.py
@@ -1,5 +1,6 @@
 import configure
-import message
+from message import Message
+from common.linktable import LinkTable
 from multiprocessing import Process, Lock
 
 
@@ -7,23 +8,54 @@ class MessageQueue():
 	def __init__(self):
 		gConfig = configure.ConfigParser.getInstance()
 		self.capacity = gConfig.getInt('system', 'max_queue_len') 
-		self.queue = [None] * self.capacity
-		self.realSize = 0
+		self.queue = LinkTable()
 		self.mutex = Lock()
+		self.current = self.queue.begin()
+		self.msgTable = {}
 		
 	
 	def push(self, msg):
 		self.mutex.acquire()
 		# push message to queue
 		# write here 
-		# this queue is a loop queue
-
+		# this queue is double link
+		node = self.queue.pushBack(msg)
+		self.msgTable[msg.msgNo] = node
 		self.mutex.release()
 
 	def pop(self):
-		pass	
+		self.mutex.acquire()
+		self.queue.popFront()
+		self.mutex.release()
+	
+	def popNode(self, node):
+		self.mutex.acquire()
+		self.queue.removeNode(node)
+		self.msgTable.pop(node.data.msgNo)
+		self.mutex.release()
+
+	def currentMessage(self):
+		return self.current.data
+
+	def nextMessage(self):
+		self.current = self.current.next.data
+		return self.current.data
+
+	def size(self):
+		return self.queue.size()
+
+	
+	def toString(self):
+		self.queue.toString()
+		print (self.msgTable)
 
 
 if __name__ == '__main__':
 	obj = MessageQueue()
-	print (obj.capacity,obj.realSize,obj.mutex)	
+	print (obj.capacity,obj.size(),obj.mutex)	
+	for i in range(0,10):
+		msg = Message('cool', i)
+		msg.setMsgNo(i)
+		obj.push(msg)
+	obj.toString()
+	
